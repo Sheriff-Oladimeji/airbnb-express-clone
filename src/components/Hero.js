2dc9d01ab58df5f4e73d834571b0fea5f24a9b5d
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-27 11:12:32 +0100

    add styling to section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,10 +2,10 @@ import hero from "../images/Group 77.png"
 
 export default  function Hero(){
 return(
-  <section>
-    <img src={hero} alt=""/>
-    <h1>Online Experiences</h1>
-    <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+  <section className="hero">
+    <img src={hero} alt="" className="hero-photo"/>
+    <h1 className="hero--header">Online Experiences</h1>
+    <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
 </p>
   </section>
 )
