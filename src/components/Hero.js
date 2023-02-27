258d42fe412df3ec33b962420ad1c44b5885df97
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-27 11:07:16 +0100

    modify files

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import hero from "../images/Group 77.png"
+
+export default  function Hero(){
+return(
+  <section>
+    <img src={hero} alt=""/>
+  </section>
+)
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+
+import logo from "../images/airbnb 1.png"
+export default function Navbar(){
+  return(
+    <nav>
+      <img src={logo} alt="" className="nav--logo"/>
+    </nav>
+  )
+}
