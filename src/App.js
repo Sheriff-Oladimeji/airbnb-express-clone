f9991620b61e724ba00747bc1ea1be6b3e87afcf
sheriff oladimeji <dimejiademola5@gmail.com>
2023-02-27 15:37:02 +0100

    modify files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/card";
 import "./styles.css"
 function App() {
   return (
    <div>
     <Navbar/>
     <Hero/>
+    <Card/>
    </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,18 @@
 import img1 from "../images/image 12.png"
-import star from "../images/star 1.png"
+import star from "../images/Star 1.png"
+
 export default function Card(){
   return(
     <div className="card">
  <img src={img1} alt=""/>
  <div className="card--stats">
   <img src={star} alt=""/>
-  <span></span>
-  <span></span>
-  <span></span>
+  <span>5.0</span>
+  <span>(6) • </span>
+  <span>USA</span>
  </div>
+ <p>Life lessons with Kattie Zaferes</p>
+ <p><span>From $136</span> / person</p>
     </div>
   )
 }
